1b5b316d6edf0cc7d7fd74cf273936da1e5067cd
Auroral <auroral_love@outlook.com>
2019-01-15 22:22:49 +0800

    init commit.

--- album/src/main/java/auroral/widget/view/album/utils/ToastManage.java
+++ album/src/main/java/auroral/widget/view/album/utils/ToastManage.java
@@ -3,12 +3,6 @@ package auroral.widget.view.album.utils;
 import android.content.Context;
 import android.widget.Toast;
 
-/**
- * @author：luck
- * @data：2018/3/28 下午4:10
- * @描述: Toast工具类
- */
-
 public final class ToastManage {
 
     public static void s(Context mContext, String s) {
